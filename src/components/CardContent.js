65c549d3f416c842ac0de59d4418b365cbe77398
Shivsay <shivsa543@gmail.com>
2023-07-30 14:28:46 +0530

    added cards and new colorscheme

---
+++ src/components/CardContent.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import img1 from '../IEDC_Nest_examle.png';
+
+function CardContent() {
+
+    return(
+            <div className="card">
+                <h1>IEDC NEST</h1>
+                <img src={img1}></img>
+                <p>
+                    Teamwork and collaboration gives rise to wonderful results. We’re looking for smart, talented and skilled innovators to be a part of the IEDC students’ team, purely led and operated by students themselves!
+
+For the very first time we're bringing in a student team for IEDC Kerala. Applications are invited from students to be a part of 𝗜𝗘𝗗𝗖 𝗡𝗘𝗦𝗧 - 𝗡𝗲𝘁𝘄𝗼𝗿𝗸 𝗼𝗳 𝗘𝗻𝘁𝗿𝗲𝗽𝗿𝗲𝗻𝗲𝘂𝗿𝘀 𝗮𝗻𝗱 𝗦𝘁𝘂𝗱𝗲𝗻𝘁 𝗧𝗲𝗮𝗺 for the year 2022-23.
+
+Why wait? Grab this opportunity to be a part of the vibrant startup ecosystem.
+                </p>
+            </div>
+    );
+}
+
+export default CardContent;
+
+
--- src/components/EventsContent.js
+++ src/components/EventsContent.js
@@ -1,9 +1,15 @@
 import React from "react"
-
+import logo from '../IEDC_Logo.png';
+import img1 from '../IEDC_Nest_examle.png';
+import CardContent from './CardContent';
 function EventsContent() {
     return (
-        <div className="card">
-        Text
+        <div className="card-content">
+         
+            <CardContent />
+            <CardContent />
+            <CardContent />
+            <CardContent />
         </div>
     );
 }
